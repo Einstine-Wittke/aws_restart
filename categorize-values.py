4c87875c70357a737844ba23e39371603cd64917
Einstine-Wittke <90602377+Einstine-Wittke@users.noreply.github.com>
2024-11-01 15:17:37 +0530

    Add files via upload

---
+++ categorize-values.py
@@ -0,0 +1,6 @@
+"""
+Your module description
+"""
+myMixedTypeList=[45,29408,1.02,True,"My name is Khan","45"]
+for item in myMixedTypeList:
+    print("{} is of the data type {}".format(item,type(item)))
---
+++ numeric-data.py
@@ -0,0 +1,20 @@
+"""
+Your module description
+"""
+print("Python has three numeric types: int,floatand complex")
+myValue=1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + "is the data type" + str(type(myValue)))
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is the data type " + str(type(myValue)))
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is the data type " + str(type(myValue)))
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
